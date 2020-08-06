0f687354db45948fefef3c83ef07c0901a0d1569
Tigransahakyan993 <tigransahakyan93@mail.ru>
2020-08-06 14:25:31 +0400

    b1

--- Sudoku/script.js
+++ Sudoku/script.js
@@ -2,6 +2,7 @@ import {account} from "./classes/Account.js";
 import {dataStore} from "./classes/DataStore.js";
 import {logIn} from "./classes/SignInView.js";
 
+//todo: test branch
 
 window.onload = main;
 
